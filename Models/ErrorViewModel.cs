9db2854d45a2fff56be5149e900d10aa9622e4ee
Patrick Ponce <patrick_ponce1@usmp.pe>
2025-08-16 17:15:52 -0500

    Rama Principal

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace programacion1_tarea5.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
